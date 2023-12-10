d2e094e50f47f3a73ae457e12244bacff684dc80
davicavalcantivital <davicvital@gmail.com>
2023-12-10 01:06:35 -0300

    okay

---
+++ src/pages/notfound/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+export default function PagDesco() {
+
+    return (
+        <section>
+            <main>
+                <h1>404</h1>
+                <p>Pagina não encontrada</p>
+            </main>
+        </section>
+    );
+}
---
+++ src/pages/pagina_principal/index.js
@@ -0,0 +1,26 @@
+import './index.scss';
+
+export default function PagPrin() {
+
+  return (
+    <section className='index_pagprin'>
+      <main className='fundo'>
+       <header className='cabecalho'>
+        <div>
+          <h1>Conversor</h1>
+        </div>
+        <div>
+          {/* <img src='assets/image/history.svg' alt='history' /> */}
+        </div>
+       </header>
+       <section className='sec-1'>
+        <div className='interface'>
+          <nav>
+            
+          </nav>
+        </div>
+       </section>
+      </main>
+    </section>
+  );
+};
